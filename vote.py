16513cd491c4a8f3e397f6f9a29ded997313954a
Colin Armstrong <coarmstrong@blackberry.com>
2014-10-20 11:31:34 -0400

    initial commit

---
+++ vote.py
@@ -0,0 +1,74 @@
+import StringIO
+import socket
+import urllib
+import pycurl
+import json
+
+import socks  # SocksiPy module
+import stem.process
+
+from stem.util import term
+
+SOCKS_PORT = 7000
+
+# Set socks proxy and wrap the urllib module
+
+socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, '127.0.0.1', SOCKS_PORT)
+socket.socket = socks.socksocket
+
+# Perform DNS resolution through the socket
+
+def getaddrinfo(*args):
+  return [(socket.AF_INET, socket.SOCK_STREAM, 6, '', (args[0], args[1]))]
+
+socket.getaddrinfo = getaddrinfo
+
+
+def query(url):
+  """
+  Uses pycurl to fetch a site using the proxy on the SOCKS_PORT.
+  """
+
+  output = StringIO.StringIO()
+
+  data = json.dumps({"token": "6615e8c4628cf84de2118028cddd4091", "voteTag": "vote"})
+  
+  query = pycurl.Curl()
+  query.setopt(pycurl.URL, url)
+  query.setopt(pycurl.PROXY, 'localhost')
+  query.setopt(pycurl.PROXYPORT, SOCKS_PORT)
+  query.setopt(pycurl.PROXYTYPE, pycurl.PROXYTYPE_SOCKS5)
+  query.setopt(pycurl.HTTPHEADER, ['Accept: application/json'])
+  #query.setopt(pycurl.WRITEFUNCTION, output.write)
+  query.setopt(pycurl.POST, 1)
+  query.setopt(pycurl.POSTFIELDS, data)
+
+  try:
+    query.perform()
+    return output.getvalue()
+  except pycurl.error as exc:
+    return "Unable to reach %s (%s)" % (url, exc)
+
+
+# Start an instance of Tor configured to only exit through Russia. This prints
+# Tor's bootstrap information as it starts. Note that this likely will not
+# work if you have another Tor instance running.
+
+def print_bootstrap_lines(line):
+  if "Bootstrapped " in line:
+    print term.format(line, term.Color.BLUE)
+
+
+print term.format("Starting Tor:\n", term.Attr.BOLD)
+
+tor_process = stem.process.launch_tor_with_config(
+  config = {
+    'SocksPort': str(SOCKS_PORT)
+  },
+  init_msg_handler = print_bootstrap_lines,
+)
+
+print term.format("\nChecking our endpoint:\n", term.Attr.BOLD)
+print term.format(query("http://www.smarttechchallenge.ca/js/vote/7879601/1"), term.Color.BLUE)
+
+tor_process.kill()  # stops tor
